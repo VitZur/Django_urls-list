0562996714ab566bab17c64aeea04aef246cb513
VitZur <131258965+VitZur@users.noreply.github.com>
2024-09-21 17:58:57 +0300

    Add files via upload

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,23 @@
+import pandas as pd
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
-
+import os
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    data = pd.read_csv(r'E:\scripts(I_I)\First_project_django\1.2-requests-templates\data.csv')
+        
+    data_list = data.to_dict(orient='records')
 
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(data_list, 10)
+    page_obj = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page_obj,
+        'page': page_obj,
     }
     return render(request, 'stations/index.html', context)
+
